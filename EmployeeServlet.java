cc1124c971b92e0eb609a3859efa6343df866859
MahendraShinde@synergetics-india.com <mahendrashinde@synergetics-india.com>
2017-02-03 12:04:03 +0530

    Modified Servlet

--- EmployeeServlet.java
+++ EmployeeServlet.java
@@ -1,11 +1,19 @@
 package com.cg.servlets;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.cg.exceptions.EMSException;
+import com.cg.models.Employee;
+import com.cg.service.EmployeeService;
+import com.cg.service.IEmployeeService;
 
 /**
  * Servlet implementation class EmployeeServlet
@@ -27,17 +35,43 @@ public class EmployeeServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String action = request.getParameter("action");
+		//Create instance of Service
+		IEmployeeService service = new EmployeeService();
+		
 		switch(action){
 		case "list":
+			try{
 			//code to list employees from service
+			List<Employee> emps=service.getAll();
 			//add list into SESSION
+			request.getSession().setAttribute("emplist", emps);
 			//forward request to "list.jsp"
+			}catch(EMSException ex){
+				request.getSession().setAttribute("error", ex.getMessage());
+			}
+			RequestDispatcher view = request.getRequestDispatcher("list.jsp");
+			view.forward(request, response);
 			return;
 		case "add":
 			//code to add employee
+			Employee e = new Employee();
+			e.setName(request.getParameter("name"));
+			e.setDesignation(request.getParameter("designation"));
+			e.setGender(request.getParameter("gender"));
+			e.setPhone(request.getParameter("phone"));
+			e.setEmail(request.getParameter("email"));
+			
 			//save using service
+			try{
+				service.add(e);
+			}catch(EMSException ex){
+				request.getSession().setAttribute("error", ex.getMessage());
+			}
 			//store error/success message in SESSION
-			//forward request to "success.jsp"
+			request.getSession().setAttribute("emp", e);
+			
+			RequestDispatcher view2 = request.getRequestDispatcher("success.jsp");
+			view2.forward(request, response);
 			return;
 		}
 	}
